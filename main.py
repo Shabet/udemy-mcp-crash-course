48e3c6ce1292a0898e814238215f3dd8a5845f9a
Shabet <pakjunyoung@nate.com>
2025-05-31 22:05:40 +0900

    Enhance main application with MCP client integration and add isort dependency

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import asyncio
 import os
 
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+from langchain_openai import ChatOpenAI
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+
 from dotenv import load_dotenv
 
 load_dotenv()
-print(os.getenv("OPENAI_API_KEY"))
+
+llm = ChatOpenAI()
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["/Users/pjy/work/udemy-mcp-crash-course/servers/weather_server.py"],
+)
 
 async def main():
     print("Hello from mcp-crash-course!")
